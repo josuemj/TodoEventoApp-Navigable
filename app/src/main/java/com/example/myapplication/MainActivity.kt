f8e7068aae8b6b0ed041420f12b95356c202388f
josuemj <98498772+josuemj@users.noreply.github.com>
2023-09-14 22:53:11 -0300

    folders removed

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,22 +19,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.myapplication.ui.theme.MyApplicationTheme
-
-
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-
-                mainScreen()
-
-            }
-        }
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
